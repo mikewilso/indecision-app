534b97742fd50b457a5b920da4ff54cff9fafe81
Michael Wilson <miwilson@sovrn.com>
2019-01-03 16:36:27 -0700

    First use of react through render function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,15 @@
 console.log('app.js is running.');
+
+// JSX - JavaScript XML
+//var template = <p>This is JSX from app.js!</p>;
+
+//Babel Translation
+var template = React.createElement(
+    'p',
+    null,
+    'This is JSX from app.js!'
+  );
+var appRoot = document.getElementById("app");
+
+//ReactDOM.render takes 2 args, the jsx template and the DOM element
+ReactDOM.render(template, appRoot);
