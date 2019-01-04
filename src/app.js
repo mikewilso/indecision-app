13389e391db11c69848e4340b5740d9a5d47cd13
Michael Wilson <miwilson@sovrn.com>
2019-01-04 09:00:05 -0700

    Added src folder, package.json dependencies

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
-"use strict";
-
 console.log('app.js is running.');
 
 // JSX - JavaScript XML
-var template = React.createElement(
-  "h1",
-  null,
-  "Changed to H1 with --watch"
-);
+var template = <h1>Changed to H1 with --watch</h1>;
 var appRoot = document.getElementById("app");
 
 //ReactDOM.render takes 2 args, the jsx template and the DOM element
