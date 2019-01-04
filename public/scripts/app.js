b44ecd3cce887049b94baff2491fddd85370b4ec
Michael Wilson <miwilson@sovrn.com>
2019-01-04 14:29:44 -0700

    Added conditional rendering, 3 ways

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,8 @@ console.log('app.js is running.');
 
 var app = {
   title: "Indecision App",
-  subtitle: "Put your life in the hands of a computer"
+  subtitle: "Put your life in the hands of a computer",
+  options: ['One', 'Two']
 
   // JSX - JavaScript XML
 };var template = React.createElement(
@@ -15,10 +16,15 @@ var app = {
     null,
     app.title
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     "p",
     null,
     app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
   ),
   React.createElement(
     "ol",
@@ -41,26 +47,33 @@ var user = {
   age: 29,
   location: 'Boulder'
 };
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  }
+}
+
 var templateTwo = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    user.name
+    user.name ? user.name : 'Anonymous'
   ),
-  React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     "p",
     null,
     "Age: ",
     user.age
   ),
-  React.createElement(
-    "p",
-    null,
-    "Location: ",
-    user.location
-  )
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById("app");
