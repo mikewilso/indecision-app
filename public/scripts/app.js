6d30706081c20a6ed86b701e349f4d091a00fbff
Michael Wilson <miwilson@sovrn.com>
2019-01-04 09:57:15 -0700

    Challenge one, make and render template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,55 @@ console.log('app.js is running.');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Changed to H1 with --watch"
+  React.createElement(
+    "h1",
+    null,
+    "Indecision App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Some more info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Michael Wilson"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 29"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Boulder"
+  )
+);
+
 var appRoot = document.getElementById("app");
 
 //ReactDOM.render takes 2 args, the jsx template and the DOM element
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 console.log('app.js is running.');
 
 // JSX - JavaScript XML
-var template = <h1>Changed to H1 with --watch</h1>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>Some more info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Michael Wilson</h1>
+    <p>Age: 29</p>
+    <p>Location: Boulder</p>  
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
 //ReactDOM.render takes 2 args, the jsx template and the DOM element
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
