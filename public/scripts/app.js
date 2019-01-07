2e7882fb8b60bc58c5fa4304c883f9917e0028f1
Michael Wilson <miwilson@sovrn.com>
2019-01-07 16:10:50 -0700

    Added functional counter w/custom render function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,49 +1,112 @@
 'use strict';
 
-// arguments object - no longer bound with arrow functions
+console.log('app.js is running.');
 
-// use ES5 functions if you need access to arguments object
-var add = function add(a, b) {
-    // console.log(arguments);
-    return a + b;
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+
+  // JSX - JavaScript XML
+};var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+// const user = {
+//     name: 'Michael',
+//     age: 29,
+//     location: 'Boulder'
+// };
+
+// function getLocation(location){
+//     if(location){
+//         return <p>Location: {location}</p>;
+//     }
+// }
+
+// const templateTwo = (
+//   <div>
+//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
+//     {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+//     {getLocation(user.location)}
+//   </div>
+// );
+
+var count = 0;
+
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
+};
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+};
+var setupReset = function setupReset() {
+  count = 0;
+  renderCounterApp();
 };
 
-console.log(add(55, 1, 1001));
+var appRoot = document.getElementById('app');
 
-// this keyword - no longer bound
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: setupReset },
+      'reset'
+    )
+  );
 
-var user = {
-    name: 'Mike',
-    cities: ['Miami', 'New York', 'Ireland'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city + "!";
-        });
-
-        // this.cities.forEach((city) => {
-        //     console.log(this.name + ' ' + 'has lived in ' + city);
-        // });
-    }
+  ReactDOM.render(templateTwo, appRoot);
 };
 
-// user.printPlacesLived();
-console.log(user.printPlacesLived());
-
-var multiplier = {
-    // numbers - array of numbers
-    numbers: [1, 2, 3, 4, 5, 6],
-    // multiplyBy - single number
-    multiplyBy: 3,
-    // multiply - return a new array where the numbers have been multiplied - map method
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
-    }
-};
-
-console.log(multiplier.multiply());
+renderCounterApp();
