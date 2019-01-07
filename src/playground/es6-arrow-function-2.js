1b329b9176f6391a9ebd6af0cb8710bf9ad888e7
Michael Wilson <miwilson@sovrn.com>
2019-01-07 10:59:04 -0700

    More advanced arrow functions in playground

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,49 +1,39 @@
-'use strict';
-
 // arguments object - no longer bound with arrow functions
 
 // use ES5 functions if you need access to arguments object
-var add = function add(a, b) {
+const add = (a, b) => {
     // console.log(arguments);
     return a + b;
-};
+}
 
 console.log(add(55, 1, 1001));
 
 // this keyword - no longer bound
 
-var user = {
+const user = {
     name: 'Mike',
     cities: ['Miami', 'New York', 'Ireland'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city + "!";
-        });
+    printPlacesLived()  {
+        return this.cities.map((city) => this.name + ' has lived in ' + city + "!");
 
         // this.cities.forEach((city) => {
         //     console.log(this.name + ' ' + 'has lived in ' + city);
         // });
     }
-};
+}
 
 // user.printPlacesLived();
 console.log(user.printPlacesLived());
 
-var multiplier = {
+const multiplier = {
     // numbers - array of numbers
     numbers: [1, 2, 3, 4, 5, 6],
     // multiplyBy - single number
     multiplyBy: 3,
     // multiply - return a new array where the numbers have been multiplied - map method
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
+    multiply () {
+        return this.numbers.map((number) => number * this.multiplyBy);
     }
-};
+}
 
-console.log(multiplier.multiply());
+console.log(multiplier.multiply()); 
