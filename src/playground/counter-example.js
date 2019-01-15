55b632aaa22216d79cbf27040f1370ac5d48f8d4
Michael Wilson <miwilson@sovrn.com>
2019-01-15 11:04:45 -0700

    alternative setState method, but might get deprecated

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -28,7 +28,19 @@ class Counter extends React.Component {
       return {
         count: 0
       }
+    });
+    //Alternative syntax that might be deprecated
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 0
+      }
     })
+    // this.setState({
+    //   count: 0
+    // });
+    // this.setState({
+    //   count: this.state.count + 1
+    // });
   }
 
   render() {
