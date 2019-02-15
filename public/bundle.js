0c58de27f6392bf7ab56df74c6768e0af71a3b47
Michael Wilson <miwilson@sovrn.com>
2019-02-15 14:51:00 -0700

    ES6 export default example

--- public/bundle.js
+++ public/bundle.js
@@ -71,16 +71,18 @@
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
 // import './utils.js';
-// import { square, add } from './utils.js';
+// import subtract, { square, add } from './utils.js';
 
 // console.log('app.js is running');
 // console.log(square(4));
 // console.log(add(100, 23));
+// console.log(subtract(100, 81));
 
 
 
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["c" /* isAdult */](18));
 console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* default */](65));
 
 /***/ }),
 /* 1 */,
@@ -89,13 +91,16 @@ console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
 
 "use strict";
 const isAdult = (age) => age >= 18;
-/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+/* harmony export (immutable) */ __webpack_exports__["c"] = isAdult;
 
 
 const canDrink = (age) => age >= 21;
 /* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
 
 
+const isSeniorCitizen = (age) => age >= 65;
+
+/* harmony default export */ __webpack_exports__["b"] = (isSeniorCitizen);
 
 
 /***/ })
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,11 @@ export const square = (x) => x * x;
 
 export const add = (a, b) => a + b;
 
+const subtract = (a, b) => a - b;
+
+// export an expression, (a, b) => a - b; also works
+export default subtract;
+
 //default export - named exports
 
-// export { square, add };
+// export { square, add, subtract as default };
