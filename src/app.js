5559a8f4540b5b1d4dc17e72765b6227a5a34a62
Michael Wilson <miwilson@sovrn.com>
2019-02-19 11:16:11 -0700

    broke out components into separate files

--- src/app.js
+++ src/app.js
@@ -1,22 +1,5 @@
-// install -> import -> use
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>THIS IS JSX FROM WEBPACK</p>
-
-ReactDOM.render(template, document.getElementById('app'));
-
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
-
-// import isSenior, { isAdult, canDrink } from './person.js';
-
-// console.log(isAdult(18));
-// console.log(canDrink(18));
-// console.log(isSenior(65));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
